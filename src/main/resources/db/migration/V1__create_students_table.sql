11a10803937463b6b5ca7c4f01d3d86555f03f09
Muriel Fritsch <fritsch.muriel@gmail.com>
2025-12-09 12:19:08 +0200

    clean up by removing trailing comma in migration script

--- src/main/resources/db/migration/V1__create_students_table.sql
+++ src/main/resources/db/migration/V1__create_students_table.sql
@@ -4,5 +4,5 @@ CREATE TABLE students
    first_name       VARCHAR(255) NOT NULL,
    last_name        VARCHAR(255) NOT NULL,
    email            VARCHAR(255) NOT NULL UNIQUE,
-   age              INTEGER NOT NULL,
+   age              INTEGER NOT NULL
 );
