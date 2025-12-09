be9282353c812caf614226ddb0b21bd1ffdd2c7d
Muriel Fritsch <fritsch.muriel@gmail.com>
2025-12-09 12:07:46 +0200

    feat: add flyway to project add V1 Flyway migration to create students table

---
+++ src/main/resources/db/migration/V1__create_students_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students
+(
+   id               BIGSERIAL PRIMARY KEY,
+   first_name       VARCHAR(255) NOT NULL,
+   last_name        VARCHAR(255) NOT NULL,
+   email            VARCHAR(255) NOT NULL UNIQUE,
+   age              INTEGER NOT NULL,
+);
